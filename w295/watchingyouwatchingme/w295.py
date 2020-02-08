74dd64655d32e45c84c4a44f1a1da8496c5ce0f4
Nugzar Nebieridze <n.nebieridze@gmail.com>
2020-02-07 21:15:00 -0800

    Started working on Sign in with Twitter

--- w295/watchingyouwatchingme/w295.py
+++ w295/watchingyouwatchingme/w295.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return redirect('https://twitter.com')
