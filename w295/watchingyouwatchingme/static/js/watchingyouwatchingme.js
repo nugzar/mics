54990b124edb0f4c809beb579b94895c8255059e
Nugzar Nebieridze <n.nebieridze@gmail.com>
2020-02-08 18:18:51 -0800

    Profile image and displaying log in div

--- w295/watchingyouwatchingme/static/js/watchingyouwatchingme.js
+++ w295/watchingyouwatchingme/static/js/watchingyouwatchingme.js
@@ -1,5 +1,13 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-    }
-})
+﻿﻿var WYWMApp = angular.module('WYWMApp', []);
+
+WYWMApp.controller('WYWMController', ['$scope','$http', '$timeout', '$filter', function ($scope, $http, $timeout, $filter) {
+
+  $scope.userprofileimage = 'static/img/noprofileimg.jpg';
+  $scope.loggedin = false;
+
+  $http.get('/userinfo').then(function(response) {
+    $scope.userprofileimage = response.data.profile_image_url_https;
+    $scope.loggedin = true;
+  });
+
+}]);
