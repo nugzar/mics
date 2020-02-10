c01bcd113bccf43acca7495a648d19487dd666b8
Nugzar Nebieridze <n.nebieridze@gmail.com>
2020-02-09 17:15:51 -0800

    Added query for likes and favorites and queueing the data to SQS

--- w295/watchingyouwatchingme/w295.py
+++ w295/watchingyouwatchingme/w295.py
@@ -1,4 +1,4 @@
-import os
+import os, boto3
 from flask import Flask, redirect, url_for, render_template
 from flask import request, session, json, send_from_directory
 from rauth import OAuth1Service
@@ -6,7 +6,6 @@ from TwitterAPI import TwitterAPI
 
 app = Flask(__name__)
 app.config.from_pyfile('w295.config', silent=True)
-
 app.secret_key = app.config['SESSION_KEY']
 
 twitter = OAuth1Service(
@@ -17,6 +16,12 @@ twitter = OAuth1Service(
     access_token_url='https://api.twitter.com/oauth/access_token',
     authorize_url='https://api.twitter.com/oauth/authorize',
     base_url='https://api.twitter.com/1.1/')
+
+sqs = boto3.client('sqs',
+    aws_access_key_id=app.config['SQS_TWEETS_ADD_KEY'],
+    aws_secret_access_key=app.config['SQS_TWEETS_ADD_SECRET'],
+    region_name=app.config['SQS_TWEETS_REGION']
+)
 
 @app.route('/')
 def index():
@@ -79,6 +84,9 @@ def userinfo(refresh=True):
     if ('userinfo' in session) and not refresh:
         return session['userinfo']
 
+    #print ('Calling userinfo:', session['consumer_key'], session['consumer_secret'],
+    #    session['access_token'], session['access_token_secret'])
+
     api = TwitterAPI(session['consumer_key'], session['consumer_secret'],
         session['access_token'], session['access_token_secret'])
 
@@ -86,8 +94,11 @@ def userinfo(refresh=True):
 
     if r.status_code == 200:
         print ("Refreshed user details")
-        session['userinfo'] = json.dumps(json.loads(r.text))
-        return session['userinfo']
+
+        userinfo_str = json.dumps(json.loads(r.text))
+        session['userinfo'] = userinfo_str
+
+        return userinfo_str
 
     return ""
 
@@ -99,12 +110,109 @@ def usertweets():
     # Let's refresh the userinfo and tokens
     userinfo(True)
 
+    #print ('Calling usertweets:', session['consumer_key'], session['consumer_secret'],
+    #    session['access_token'], session['access_token_secret'])
+
     api = TwitterAPI(session['consumer_key'], session['consumer_secret'],
         session['access_token'], session['access_token_secret'])
 
     r = api.request('statuses/user_timeline', {'count':1000, 'screen_name': json.loads(session['userinfo'])['screen_name']})
 
     if r.status_code == 200:
-        return json.dumps(json.loads(r.text))
+        tweets = json.loads(r.text)
+        chunk_size = 25
+
+        for n in range(0, len(tweets), chunk_size):
+            response = sqs.send_message(
+                QueueUrl=app.config['SQS_TWEETS_ENDPOINT'],
+                MessageBody=json.dumps(tweets[n:n+chunk_size]), 
+                MessageAttributes={
+                'Sender': {
+                    'StringValue': 'usertweets',
+                    'DataType': 'String'
+                }
+            })
+            print ('SQS Add usertweets:', n, n+chunk_size, response)
+
+        return json.dumps(tweets)
+
+    return ""
+
+@app.route('/userfriends')
+def userfriends():
+    if 'access_token' not in session:
+        return ""
+
+    # Let's refresh the userinfo and tokens
+    userinfo()
+
+    #print ('Calling userlikes:', session['consumer_key'], session['consumer_secret'],
+    #    session['access_token'], session['access_token_secret'])
+
+    api = TwitterAPI(session['consumer_key'], session['consumer_secret'],
+        session['access_token'], session['access_token_secret'])
+
+    r = api.request('friends/ids')
+
+    if r.status_code == 200:
+        friends = json.loads(r.text)
+        chunk_size = 1000
+        message = {}
+
+        message["userid_str"] = json.loads(session['userinfo'])["id_str"]
+
+        for n in range(0, len(friends["ids"]), chunk_size):
+            message["ids"] = friends["ids"][n:n+chunk_size]
+
+            response = sqs.send_message(
+                QueueUrl=app.config['SQS_FRIENDS_ENDPOINT'],
+                MessageBody=json.dumps(message), 
+                MessageAttributes={
+                'Sender': {
+                    'StringValue': 'userfriends',
+                    'DataType': 'String'
+                }
+            })
+            print ('SQS Add userfriends:', n, n+chunk_size, response)
+
+        return json.dumps(friends)
+
+    return ""
+
+@app.route('/userfavorites')
+def userfavorites():
+    if 'access_token' not in session:
+        return ""
+
+    # Let's refresh the userinfo and tokens
+    userinfo()
+
+    api = TwitterAPI(session['consumer_key'], session['consumer_secret'],
+        session['access_token'], session['access_token_secret'])
+
+    r = api.request('favorites/list', {'count':200, 'include_entities':True, 'screen_name': json.loads(session['userinfo'])['screen_name']})
+
+    if r.status_code == 200:
+        likes = json.loads(r.text)
+        chunk_size = 25
+        message = {}
+
+        message["userid_str"] = json.loads(session['userinfo'])["id_str"]
+
+        for n in range(0, len(likes), chunk_size):
+            message["favorites"] = likes[n:n+chunk_size]
+
+            response = sqs.send_message(
+                QueueUrl=app.config['SQS_LIKES_ENDPOINT'],
+                MessageBody=json.dumps(message), 
+                MessageAttributes={
+                'Sender': {
+                    'StringValue': 'userfavorites',
+                    'DataType': 'String'
+                }
+            })
+            print ('SQS Add userfavorites:', n, n+chunk_size, response)
+
+        return json.dumps(likes)
 
     return ""
