bdb7c9176e36b24370a6ca3deb8eb21ca15439ae
Nugzar Nebieridze <n.nebieridze@gmail.com>
2020-03-11 13:15:16 -0700

    Added list of influencers

--- w295/watchingyouwatchingme/w295.py
+++ w295/watchingyouwatchingme/w295.py
@@ -44,6 +44,10 @@ def index():
 
     return render_template('index.html')
 
+@app.route('/influencers')
+def influencers():
+    return render_template('influencers.html', influencers=pageranks)
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static/img'),
